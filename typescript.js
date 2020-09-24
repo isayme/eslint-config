f46cd655e2d4b383185c61bdfb9412c96ce3b1c8
isayme <isaymeorg@gmail.com>
2020-09-24 21:54:55 +0800

    add eslint-plugin-import

--- typescript.js
+++ typescript.js
@@ -1,9 +1,10 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
     "prettier/standard",

db869cd3a79daf2c1825470f0309b585a2c52bbd
isayme <isaymeorg@gmail.com>
2020-09-24 20:43:13 +0800

    update

--- typescript.js
+++ typescript.js
@@ -4,8 +4,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
     "prettier/standard",
   ],
   env: {
